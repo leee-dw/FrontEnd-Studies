9143345d2b19bcfc14717ccd737954daf24284de
likedemian <likedemian@me.com>
2017-09-17 21:46:20 +0900

    Todo list 다시 제작 && Sketch 연습

--- 2017-09-17-Todo-List/resources/js/main.js
+++ 2017-09-17-Todo-List/resources/js/main.js
@@ -3,17 +3,21 @@ var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem(
   completed: []
 };
 
-
-// Remove and Complete SVG 아이콘
-var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"></rect><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6L16.3,18.7L16.3,18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8C7.4,10.2,7.7,10,8,10c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
+// Remove & Complete SVG Icon
+var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6L16.3,18.7L16.3,18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8C7.4,10.2,7.7,10,8,10c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
 var completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
 
 renderTodoList();
 
+// 텍스트 작성하고, 버튼 글릭하면 텍스트가 리스트에 추가됨.
 document.getElementById('add').addEventListener('click', function() {
   var value = document.getElementById('item').value;
   if (value) {
-    addItem(value);
+    addItemToDOM(value);
+    document.getElementById('item').value = '';
+
+    data.todo.push(value);
+    dataObjectUpdated()
   }
 });
 
@@ -21,30 +25,27 @@ document.getElementById('item').addEventListener('keydown', function(e) {
   var value = this.value;
   if (e.code === 'Enter' && value) {
     addItem(value);
-  };
-})
+  }
+});
 
 function addItem(value) {
-  addItemToDOM (value);
+  addItemToDOM(value);
   document.getElementById('item').value = '';
   data.todo.push(value);
-  dataObjectUpdated();
+  dataObjectUpdated()
 }
 
-
 function renderTodoList() {
-  if (!data.todo.length && !data.completed.length) {
-    return
-  };
+  if (!data.todo.length && !data.completed.length) return;
 
   for (var i = 0; i < data.todo.length; i++) {
     var value = data.todo[i];
-    addItemToDOM (value);
+    addItemToDOM(value);
   }
 
   for (var j = 0; j < data.completed.length; j++) {
     var value = data.completed[j];
-    addItemToDOM (value, true);
+    addItemToDOM(value, true);
   }
 }
 
@@ -82,17 +83,20 @@ function completeItem() {
     data.completed.splice(data.completed.indexOf(value), 1);
     data.todo.push(value);
   }
-  dataObjectUpdated();
+  dataObjectUpdated()
 
-  var target = (id === 'todo') ? document.getElementById('completed') : document.getElementById('todo');
+  // 아이템이 완료됐는지 아닌지 체크 
+  var target = (id === 'todo') ?
+    document.getElementById('completed') :
+    document.getElementById('todo');
 
   parent.removeChild(item);
   target.insertBefore(item, target.childNodes[0]);
 }
 
 
-// 투 두 리스트에 아이템 추가
-function addItemToDOM (text, completed) {
+// 리스트에 아이템을 추가하는 함수
+function addItemToDOM(text, completed) {
   var list = (completed) ? document.getElementById('completed') : document.getElementById('todo');
 
   var item = document.createElement('li');
@@ -105,14 +109,14 @@ function addItemToDOM (text, completed) {
   remove.classList.add('remove');
   remove.innerHTML = removeSVG;
 
-  // 클릭하면 제거됨.
+
+  // 이벤트를 클릭하면 아이템이 지워짐.
   remove.addEventListener('click', removeItem);
 
   var complete = document.createElement('button');
   complete.classList.add('complete');
   complete.innerHTML = completeSVG;
 
-  // 클릭하면 완료 표시됨
   complete.addEventListener('click', completeItem);
 
   buttons.appendChild(remove);
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FrontEnd-Studies
+> Make Sth with HTML5 & CSS & Javascript
+
+## 목표 
+ - 자바스크립트를 활용한 미니 프로젝트를 진행한다.
