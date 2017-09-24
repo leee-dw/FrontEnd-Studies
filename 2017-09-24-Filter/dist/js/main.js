f20380e6b32f1943f96b3f3ae61d48373a7051a2
likedemian <likedemian@me.com>
2017-09-24 17:52:35 +0900

    왜 안됨?

--- 2017-09-24-Filter/dist/js/main.js
+++ 2017-09-24-Filter/dist/js/main.js
@@ -1,10 +1,12 @@
 var sortCategories = function() {
+
   var categories = document.querySelectorAll('.category');
   var showAll = document.getElementById('showall')
 
   for (var i=0; i<categories.length; i++) {
 
     categories[i].addEventListener('click', function(e) {
+      
       var post = document.querySelectorAll('.post');
       var getId = e.target.id;
       var getCurrent = document.querySelectorAll('.posts .' + getId);
@@ -12,13 +14,19 @@ var sortCategories = function() {
       if (getId === 'showAll') {
 
         for (var i = 0; i < post.length; i++) {
-          post[i].classList.remove('hidden')
+
+          post[i].classList.remove('hidden');
+
         }
+
       } else {
+
         var s = getCurrent[0].className;
-        var getCurrentClasName = s.substring(0, s.indexOf(''));
+        var getCurrentClassName = s.substring(0, s.indexOf(''));
+
         for (var i = 0; i < getCurrent.length; i++) {
           getCurrent[i].classList.remove('hidden')
+
         }
         for (var i = 0; i < getCurrent.length; i++) {
           if (post[i].classList.contains(getCurrentClassName)) {
