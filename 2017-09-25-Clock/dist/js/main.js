82121b6c4e29f647c5fe39bd7983c0c586ee21ff
likedemian <likedemian@me.com>
2017-09-25 12:11:37 +0900

    Clock which changed by time

---
+++ 2017-09-25-Clock/dist/js/main.js
@@ -0,0 +1,23 @@
+function clock() {
+  var date = new Date(),
+    min = date.getMinutes(),
+    sec = date.getSeconds(),
+    hr = date.getHours();
+  if (hr < 10) {
+    hr = "0" + hr;
+  }
+  if (min < 10) {
+    min = "0" + min;
+  }
+  if (sec < 10) {
+    sec = "0" + sec;
+  }
+  var color = "" + sec + min + hr;
+  document.getElementById('clock').innerHTML = hr + ":" + min + ":" + sec;
+  document.body.style.background = "#" + color;
+
+}
+
+setInterval(function() {
+  clock()
+}, 100);
