1759e49c1af8d567bdedcd305c0f9322cc7cf60f
likedemian <likedemian@me.com>
2017-09-28 16:59:49 +0900

    FX MAIN 완성

--- 2017-09-26-FX/dist/js/parallax.js
+++ 2017-09-26-FX/dist/js/parallax.js
@@ -52,4 +52,30 @@ $(window).scroll(function() {
 
     $('.dot').css({ 'transition': 'all 0.5s' });
   }
+
+  if (wScroll > $('.third-pics').offset().top / 1.2) {
+    $('.dot').css({ 'borderColor': '#fff' });
+    $('.dot').mouseover(function() {
+      $(this).css({ 'backgroundColor': '#fff' })
+    });
+    $('.dot').mouseout(function() {
+      $('.dot').css({ 'backgroundColor': '' })
+    });
+
+    $('.dot').css({ 'transition': 'all 0.5s' });
+  }
+
+  if (wScroll > $('.content-3').offset().top / 1.18) {
+    $('.dot').css({ 'borderColor': '#000' });
+    $('.dot').mouseover(function() {
+      $(this).css({ 'backgroundColor': '#000' })
+    });
+    $('.dot').mouseout(function() {
+      $('.dot').css({ 'backgroundColor': '' })
+    });
+
+    $('.dot').css({ 'transition': 'all 0.5s' });
+  }
+
+
 });
