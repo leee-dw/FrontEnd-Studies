3f4a3bb6371618daa1bd89c91108ae24a2d3dcc3
likedemian <likedemian@me.com>
2017-09-29 22:50:34 +0900

    Attendance 수정

--- 2017-09-27-Attandance-App/dist/js/main.js
+++ 2017-09-27-Attandance-App/dist/js/main.js
@@ -7,14 +7,27 @@ function checkDate() {
   var day = new Date();
   var localeDate = day.toLocaleDateString();
   var localeTime = day.toLocaleTimeString();
-  
-  var check = document.getElementById('check')
-  var item = document.createElement('li');
-  var parent = check.parentNode
 
-  // 이.....이거냐?
-  item.innerText = localeDate + localeTime
-  parent.appendChild(item)
+  var months = day.getMonth();
+  var hours = day.getHours();
+  var mins = day.getMinutes();
+  var secs = day.getSeconds();
+
+  var check = document.getElementById('check');
+  var item = document.createElement('li');
+  var parent = check.parentNode;
+
+
+  item.classList.add('notification');
+  item.classList.add('column');
+
+
+    item.classList.add('is-offset-1');
+    item.classList.add('is-2');
+
+  // item.classList.add('is-2');
+  item.innerHTML = localeDate + " " + localeTime
+  check.appendChild(item)
 
 }
 
